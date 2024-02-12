1008cc126f331b9cdbeecc570164659e91a9d4db
ftarhon <florin.titu@gmail.com>
2024-02-12 19:48:52 +0200

    Add a graph

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+    'Series_1': [1, 3, 4, 5, 7],
+    'Series_2': [10, 30, 40, 100, 250],
+
+}
+
+df = pd.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
@@ -6,3 +15,4 @@ st.write('''
 This is our first Web App.
 Enjoy it!
 ''')
+st.write(df)
